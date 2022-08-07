b981e84ff8a9d043d887c8c714fd7163cd3b7542
Pavel Kim <hello@pavelkim.com>
2022-08-07 14:59:18 +0200

    [Change] Not going to standardise labels for now

---
+++ sbin/centos7/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# TDLib dependencies for rhel7
+#
+
+set -e
+
+echo "Start building"
+
+cd td
+
+rm -vrf build
+mkdir build
+
+cd build
+
+CC=/opt/rh/devtoolset-9/root/usr/bin/gcc CXX=/opt/rh/devtoolset-9/root/usr/bin/g++ cmake3 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
+cmake3 --build . --target prepare_cross_compiling
+
+cd ..
+php SplitSource.php
+
+cd build
+cmake3 --build . --target install
+
+cd ..
+php SplitSource.php --undo
+
+cd ..
+ls -l td/tdlib
+
+echo "Finished building"
