88ad7de889d86fda57b47e42e81db85456b8593f
Pavel Kim <hello@pavelkim.com>
2023-05-13 05:49:30 +0200

    [Add] Docker build process implemented

---
+++ sbin/macos-13/install-dependencies-docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# TDLib dependencies for Docker build on macos-13
+#
+
+set -e
+
+echo "Start installing dependencies for Docker build"
+
+if xcode-select --install; then
+	echo "xcode successfully installed"
+else
+	echo "xcode is probably already there. not taking any more effort to verify what has happened"
+fi
+
+echo "Downloading Docker package"
+curl -o /tmp/Docker.dmg -Ls https://desktop.docker.com/mac/main/arm64/Docker.dmg
+
+echo "Installing Docker"
+hdiutil attach Docker.dmg
+/Volumes/Docker/Docker.app/Contents/MacOS/install
+hdiutil detach /Volumes/Docker
+
+if docker --version; then
+	echo "Docker seems to be installed and ready to use"
+else
+	echo "Error installing docker" >&2
+	exit 2
+fi
+
+echo "Finished installing dependencies for Docker build"
