593c81ea1f3b02202ba78c4ca9d9384ee4d2cfb3
Pavel Kim <hello@pavelkim.com>
2023-05-13 20:51:09 +0200

    [Change] More debuggable install-dependencies-docker script

--- sbin/macos-13/install-dependencies-docker.sh
+++ sbin/macos-13/install-dependencies-docker.sh
@@ -5,27 +5,72 @@
 
 set -e
 
-echo "Start installing dependencies for Docker build"
+log(){
+	echo "[$(date +%F +%T)] $*"
+}
+
+log "Start installing dependencies for Docker build"
 
 if xcode-select --install; then
-	echo "xcode successfully installed"
+	log "xcode successfully installed"
 else
-	echo "xcode is probably already there. not taking any more effort to verify what has happened"
+	log "xcode is probably already there. not taking any more effort to verify what has happened"
 fi
 
-echo "Downloading Docker package"
+log "Downloading Docker package"
 curl -o /tmp/Docker.dmg -Ls https://desktop.docker.com/mac/main/arm64/Docker.dmg
 
-echo "Installing Docker"
+log "Attaching Docker.dmg"
 hdiutil attach /tmp/Docker.dmg
+
+log "Installing Docker"
 /Volumes/Docker/Docker.app/Contents/MacOS/install
+
+log "Starting Docker desktop"
+open -a Docker
+
+log "Workaroundig probably missing PATH section"
+export PATH="/usr/local/bin:${PATH}"
+
+log "Verifying docker binary is there"
+if which docker; then
+	log "Found docker binary"
+else
+	log "Nothing found, that's probably bad"
+fi
+
+log "Waiting for docker ps to work"
+MAX_ATTEMPTS=15
+CURRENT_ATTEMPT=0
+DELAY_SEC=2
+DOCKER_CHECK_RC=1
+
+while [[ "${CURRENT_ATTEMPT}" < "${MAX_ATTEMPTS}" ]]; do
+	log "Attempt ${CURRENT_ATTEMPT} executing docker ps"
+
+	if docker ps; then
+		log "Docker daemon is up and running"
+		DOCKER_CHECK_RC=0
+		break
+	else
+		log "Docker daemon doesn't seem to be up (sleep ${DELAY_SEC} sec.)"
+		sleep "${DELAY_SEC}"	
+	fi
+done
+
+if [[ "${DOCKER_CHECK_RC}" != 0 ]]; then 
+	log "Error probing Docker daemon: MAX_ATTEMPTS=${MAX_ATTEMPTS} CURRENT_ATTEMPT=${CURRENT_ATTEMPT}"
+	exit "${DOCKER_CHECK_RC}"
+fi
+
+log "Detaching Docker.dmg"
 hdiutil detach /Volumes/Docker
 
 if docker --version; then
-	echo "Docker seems to be installed and ready to use"
+	log "Docker seems to be installed and ready to use"
 else
-	echo "Error installing docker" >&2
+	log "Error installing docker" >&2
 	exit 2
 fi
 
-echo "Finished installing dependencies for Docker build"
+log "Finished installing dependencies for Docker build"
